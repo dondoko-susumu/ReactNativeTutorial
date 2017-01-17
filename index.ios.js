821bf4d9eb32085c65ad9e48988c069981e968df
Ken Kubota <kkbt2003@gmail.com>
2017-01-17 12:34:42 +0900

    :+1: Props

--- index.ios.js
+++ index.ios.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { AppRegistry, Image } from 'react-native';
+import { AppRegistry, Text, View , Image } from 'react-native';
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
 
 class ReactNativeTutorial extends Component {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
     return (
-      <Image source={pic} style={{width: 193, height: 110}}/>
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
     );
   }
 }
