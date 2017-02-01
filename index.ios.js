41465ad3f8e08cbca3bd54021cc920271aaa1241
Ken Kubota <kkbt2003@gmail.com>
2017-02-01 15:06:00 +0900

    TabBarIOS hide

--- index.ios.js
+++ index.ios.js
@@ -1,38 +1,195 @@
+'use strict';
+
 import React, { Component } from 'react';
-import { AppRegistry, Navigator } from 'react-native';
 
-import MyScene from './MyScene';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TabBarIOS,
+  TouchableHighlight,
+  StatusBar,
+} from 'react-native';
 
-class SimpleNavigationApp extends Component {
-  render() {
+
+var SampleApp = React.createClass({
+  getInitialState: function() {
+    return {
+      showTabBar: true
+    };
+  },
+
+  _toggleTabBar: function() {
+  	if ( this.state.showTabBar === true ) {
+      this.setState( {showTabBar: false} );
+    }
+    else {
+      this.setState( {showTabBar: true} );
+    }
+  },
+
+  _renderTabBar: function() {
+  },
+
+  render: function() {
+
+    if (this.state.showTabBar) {
+      return (<TabBarExample toggleTabBar={this._toggleTabBar} />);
+    }
+    else {
+      return (
+        <MyViewFullScreen toggleTabBar={this._toggleTabBar} />
+      );
+    }
+
+	}
+});
+
+
+var TabBarExample = React.createClass({
+  statics: {
+    title: '<TabBarIOS>',
+    description: 'Tab-based navigation.',
+  },
+
+  displayName: 'TabBarExample',
+
+  getInitialState: function() {
+    return {
+      selectedTab: 'blueTab',
+      notifCount: 0,
+      presses: 0,
+    };
+  },
+
+  _renderContent: function(color: string, pageText: string, num?: number) {
     return (
-      <Navigator
-        initialRoute={{ title: 'My Initial Scene', index: 0 }}
-        renderScene={(route, navigator) =>
-          <MyScene
-            title={route.title}
+      <View style={[styles.tabContent, {backgroundColor: color}]}>
+        <Text style={styles.tabText}>{pageText}</Text>
+        <Text style={styles.tabText}>{num} re-renders of the {pageText}</Text>
+      </View>
+    );
+  },
 
-            // Function to call when a new scene should be displayed
-            onForward={() => {
-              const nextIndex = route.index + 1;
-              navigator.push({
-                title: 'Scene ' + nextIndex,
-                index: nextIndex,
-              });
-            }}
+  render: function() {
+    return (
+      <TabBarIOS
+        tintColor="white"
+        barTintColor="darkslateblue"
+	      translucent={true}>
 
-            // Function to call to go back to the previous scene
-            onBack={() => {
-              if (route.index > 0) {
-                navigator.pop();
-              }
-            }}
-          />
-        }
-      />
-    )
+        <TabBarIOS.Item
+          title="Blue Tab"
+          systemIcon="search"
+          selected={this.state.selectedTab === 'blueTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'blueTab',
+            });
+          }}>
+          <MyViewOne toggleTabBar={this.props.toggleTabBar} />
+        </TabBarIOS.Item>
+
+        <TabBarIOS.Item
+          title="Red Tab"
+          systemIcon="history"
+          badge={this.state.notifCount > 0 ? this.state.notifCount : undefined}
+          selected={this.state.selectedTab === 'redTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'redTab',
+              notifCount: this.state.notifCount + 1,
+            });
+          }}>
+          {this._renderContent('#783E33', 'Red Tab', this.state.notifCount)}
+        </TabBarIOS.Item>
+
+        <TabBarIOS.Item
+          systemIcon="contacts"
+          title="More Green"
+          selected={this.state.selectedTab === 'greenTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'greenTab',
+              presses: this.state.presses + 1
+            });
+          }}>
+          {this._renderContent('#21551C', 'Green Tab', this.state.presses)}
+        </TabBarIOS.Item>
+
+      </TabBarIOS>
+    );
   }
-}
+});
 
-AppRegistry.registerComponent('ReactNativeTutorial', () => SimpleNavigationApp);
-//AppRegistry.registerComponent('ReactNativeTutorial', () => PizzaTranslator);
+
+var MyViewOne = React.createClass({
+  render: function() {
+
+    StatusBar.setHidden(false, 'slide')
+
+    return (
+      <View style={[styles.tabContent, {backgroundColor: 'orange'}]}>
+        <Text style={styles.tabText}>I like Iron Maiden, 666</Text>
+      	<MyScreenToggleButton toggleTabBar={this.props.toggleTabBar} />
+      </View>
+    );
+  }
+});
+
+
+var MyViewFullScreen = React.createClass({
+  render: function() {
+
+    StatusBar.setHidden(true, 'fade')
+
+    return (
+      <View style={[styles.tabContent, {backgroundColor: 'grey'}]}>
+        <Text style={styles.tabText}>This should be full screen i.e. No TabBar.</Text>
+      	<MyScreenToggleButton toggleTabBar={this.props.toggleTabBar} />
+      </View>
+    );
+  }
+});
+
+
+var MyScreenToggleButton = React.createClass({
+  _onPressButton: function() {
+  	this.props.toggleTabBar();
+  },
+
+  render: function() {
+    return (
+      <TouchableHighlight onPress={this._onPressButton}>
+        <Text style={styles.button}>
+  				Push Me
+  			</Text>
+      </TouchableHighlight>
+    );
+  }
+});
+
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  tabContent: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  tabText: {
+    color: 'white',
+    margin: 50,
+  },
+  button: {
+    backgroundColor: 'green',
+    margin: 10,
+  }
+});
+
+AppRegistry.registerComponent('ReactNativeTutorial', () => SampleApp);
