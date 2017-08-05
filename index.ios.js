f5a4d66d034178d4dcadfff07b2ce1284746455e
Ken Kubota <kkbt2003@gmail.com>
2017-08-05 22:52:01 +0900

    update

--- index.ios.js
+++ index.ios.js
@@ -6,14 +6,35 @@ class JustifyContentBasics extends Component {
     return (
       // Try setting `justifyContent` to `center`.
       // Try setting `flexDirection` to `row`.
+      //justifyContent: 'space-around',
+      // justifyContent: 'center',
+      // alignItems: 'flex-start',
       <View style={{
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'space-around',
+        justifyContent: 'space-between',
+        alignItems: 'center',
       }}>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+        <View style={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+        }}>
+          <View style={{width: 50, height: 50, backgroundColor: 'red'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'green'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'blue'}} />
+        </View>
+        <View style={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+        }}>
+          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+        </View>
       </View>
     );
   }
