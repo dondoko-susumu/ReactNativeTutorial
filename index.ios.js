3f54fb0bf29ebc431ff15512e75aab0d1e9c9768
Ken Kubota <kkbt2003@gmail.com>
2017-08-06 21:20:24 +0900

    add eslint

--- index.ios.js
+++ index.ios.js
@@ -6,7 +6,7 @@ class JustifyContentBasics extends Component {
     return (
       // Try setting `justifyContent` to `center`.
       // Try setting `flexDirection` to `row`.
-      //justifyContent: 'space-around',
+      // justifyContent: 'space-around',
       // justifyContent: 'center',
       // alignItems: 'flex-start',
       <View style={{
@@ -14,30 +14,33 @@ class JustifyContentBasics extends Component {
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-      }}>
+      }}
+      >
         <View style={{
           flex: 1,
           flexDirection: 'row',
           justifyContent: 'flex-start',
           alignItems: 'center',
-        }}>
-          <View style={{width: 50, height: 50, backgroundColor: 'red'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'green'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'blue'}} />
+        }}
+        >
+          <View style={{ width: 50, height: 50, backgroundColor: 'red' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'green' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'blue' }} />
         </View>
         <View style={{
           flex: 1,
           flexDirection: 'row',
           justifyContent: 'flex-end',
           alignItems: 'center',
-        }}>
-          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-          <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+        }}
+        >
+          <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'skyblue' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
         </View>
       </View>
     );
   }
-};
+}
 
 AppRegistry.registerComponent('ReactNativeTutorial', () => JustifyContentBasics);
